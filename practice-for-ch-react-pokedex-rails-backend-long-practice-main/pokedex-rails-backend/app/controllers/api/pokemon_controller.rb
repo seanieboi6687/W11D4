4a367b9ad281f0e5eb0410d76be2b82573222c0c
Sean <pca.sean.jeun@gmail.com>
2023-09-07 17:21:28 -0400

    routes added

---
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,19 @@
+class Api::PokemonController < ApplicationController
+
+    def types
+        @types = Pokemon.all.select("poke_type").group("poke_type")
+        @types_arr = @types.map do |type|
+            type['poke_type']
+        end
+
+        render json: @types_arr
+    end
+
+    def index
+
+    end
+
+    def show
+
+    end
+end
--- practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
+++ practice-for-ch-react-pokedex-rails-backend-long-practice-main/pokedex-rails-backend/config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    get '/pokemon/types', to: 'pokemon#types', as: 'pokemon_types'
+    resources :pokemon, only: [:index, :create, :show, :update]
+    resources :items, only: [:index, :create, :destroy, :update]
+  end
 end
